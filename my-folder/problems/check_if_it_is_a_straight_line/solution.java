4a121265536049f7df89cd26c36f015a89f08bdf
Chandu kousik Ponnaganti <108112556+chandu736@users.noreply.github.com>
2023-06-05 05:10:48 +0000

    Sync LeetCode submission - Check If It Is a Straight Line (java)

---
+++ my-folder/problems/check_if_it_is_a_straight_line/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkStraightLine(int[][] c) {
+        int xC=c[1][0]-c[0][0];
+        int yC=c[1][1]-c[0][1];
+        for(int i=1;i<c.length;i++){
+            int x=c[i][0]-c[i-1][0];
+            int y=c[i][1]-c[i-1][1];
+            if(y*xC!=x*yC) return false;
+        }
+        return true;
+    }
+}
