0e36a525e39e067fc384d23a3913a376fd40967f
Chandu kousik Ponnaganti <108112556+chandu736@users.noreply.github.com>
2023-07-14 07:01:03 +0000

    Sync LeetCode submission - Complex Number Multiplication (java)

---
+++ my-folder/problems/complex_number_multiplication/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String complexNumberMultiply(String num1, String num2) {
+       StringBuilder sb=new StringBuilder();
+       int a=Integer.parseInt(num1.substring(0,num1.indexOf('+')));
+       int b=Integer.parseInt(num1.substring(num1.indexOf('+')+1,num1.indexOf('i')));
+        int c=Integer.parseInt(num2.substring(0,num2.indexOf('+')));
+       int d=Integer.parseInt(num2.substring(num2.indexOf('+')+1,num2.indexOf('i')));
+       sb.append(a*c-b*d);
+       sb.append('+'+"");
+       sb.append(b*c+a*d);
+       sb.append('i');
+       return sb.toString();
+    }
+}
