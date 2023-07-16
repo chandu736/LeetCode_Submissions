f3e4a81e45242fafb0a0713db5cb1901d040df5a
Chandu kousik Ponnaganti <108112556+chandu736@users.noreply.github.com>
2023-07-16 19:01:01 +0000

    Sync LeetCode submission - Remove Trailing Zeros From a String (java)

---
+++ my-folder/problems/remove_trailing_zeros_from_a_string/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String removeTrailingZeros(String num) {
+        int n=num.length()-1;
+        for(int i=n;i>0;i--){
+            if(num.charAt(i)!='0'){
+                break;
+            }
+            n--;
+        }
+        return num.substring(0,n+1);
+    }
+}
