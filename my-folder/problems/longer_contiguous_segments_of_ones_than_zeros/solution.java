cc98c5474714d07d7fbb923b86c8e64be858f83e
Chandu kousik Ponnaganti <108112556+chandu736@users.noreply.github.com>
2023-08-19 06:47:03 +0000

    Sync LeetCode submission - Longer Contiguous Segments of Ones than Zeros (java)

---
+++ my-folder/problems/longer_contiguous_segments_of_ones_than_zeros/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkZeroOnes(String s) {
+        int n=s.length();
+        int max0=0;
+        int max1=0;
+        int count0=0;
+        int count1=0;
+        for(int i=0;i<n;i++){
+            char c=s.charAt(i);
+            if(c=='0'){
+                count0++;
+                max1=Math.max(count1,max1);
+                count1=0;
+            }else{
+                count1++;
+                max0=Math.max(count0,max0);
+                count0=0;
+            }
+        }
+        max1=Math.max(max1,count1);
+        max0=Math.max(max0,count0);
+        return max1>max0;
+    }
+}
