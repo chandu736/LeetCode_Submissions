42cc2c7cb9cc5c818cd189ff54e91466c64f00da
Chandu kousik Ponnaganti <108112556+chandu736@users.noreply.github.com>
2023-08-25 10:03:26 +0000

    Sync LeetCode submission - Repeated Substring Pattern (java)

---
+++ my-folder/problems/repeated_substring_pattern/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n=s.length();
+        for(int i=1;i<=n/2;i++){
+            if(n%i==0){
+                String subString=s.substring(0,i);
+                StringBuilder repeated=new StringBuilder();
+                for(int j=0;j<n/i;j++){
+                    repeated.append(subString);
+                }
+                if(repeated.toString().equals(s)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
